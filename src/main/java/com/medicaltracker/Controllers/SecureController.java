0779b3721b322b39beb060d13bd5d14ab92d145e
Shubham Sharma <shubham.sharma1@hostbooks.com>
2024-05-01 02:56:16 +0530

    moved class

--- src/main/java/com/medicaltracker/Controllers/SecureController.java
+++ src/main/java/com/medicaltracker/Controllers/SecureController.java
@@ -1,4 +1,4 @@
-package com.medicaltracker;
+package com.medicaltracker.Controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
