cf8a7502ddc958e9d5ef48404cb13125b551c6af
Shubham Sharma <shubham.sharma1@hostbooks.com>
2024-05-01 00:32:37 +0530

    Initial Commit

---
+++ src/main/java/com/medicaltracker/MedicalTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medicaltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medicaltracker/MedicalTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicaltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
